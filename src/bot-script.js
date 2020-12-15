f6ba81fd9039cabfbff4c92aef50558f98939fa7
Lorenz Herbst <lorenz@MacBook-Air-von-Lorenz.local>
2020-12-15 21:55:57 +0100

    added a promp for setting the user name and added another request after choosing veggi

--- src/bot-script.js
+++ src/bot-script.js
@@ -1,14 +1,32 @@
 var botui = new BotUI('TestBot')
 
+
 botui.message.add({
     loading: true,
     delay: 1000,
     content: 'Welcome to my Website?'
 }).then(function() {
     return botui.message.add({
+        laoding: true,
+        delay: 500,
+        content: 'Whats your name?'
+    })
+}).then(function() {
+    return botui.action.text({
+        action: {
+            palceholder: 'Your name'
+        }
+    })
+}).then(function(res) {
+    this.name = res.value
+    botui.message.add({
+        content: `Hi ${name}`
+    })
+}).then(function() {
+    return botui.message.add({
         delay: 500,
         loading: true,
-        content: 'What do you want to have for lunch today?'
+        content: `What do you want to have for lunch today?`
     })
 }).then(function() {
     return botui.action.button({
@@ -27,14 +45,49 @@ botui.message.add({
     let message
 
     if (res.value === 'meat') {
-        message = 'Good choice! [Lets get on!](https://www.chefkoch.de/rs/s0t16/Fleisch-Rezepte.html)'
+        message = 'Good choice! [Go get some!](https://www.chefkoch.de/rs/s0t16/Fleisch-Rezepte.html)'
+        return botui.message.add({
+            delay: 1000,
+            loading: true,
+            content: message
+        })
     } else if (res.value === 'veggi') {
-        message = 'Are you shure? [Here are some ideas!](https://www.chefkoch.de/rs/s0t32/Vegetarisch-Rezepte.html)'
+        message = 'Are you shure?'
+        return botui.message.add({
+            delay: 1000,
+            loading: true,
+            content: message
+        }).then(function () {
+            return botui.action.button({
+                action: [
+                    {
+                        text: 'Id better go with meat',
+                        value: 'meat'
+                    },
+                    {
+                        text: 'Gimme veggi!',
+                        value: 'veggi'
+                    }
+                ]
+            })
+        }).then(function(res) {
+            let msg
+
+            if (res.value === 'meat') {
+                msg = 'Good choice! [Go get some!](https://www.chefkoch.de/rs/s0t16/Fleisch-Rezepte.html)'
+                return botui.message.add({
+                    delay: 1000,
+                    loading: true,
+                    content: msg
+                })
+            } else if (res.value === 'veggi') {
+                msg = 'Ok! [Here you go!](https://www.chefkoch.de/rs/s0t32/Vegetarisch-Rezepte.html)'
+                return botui.message.add({
+                    delay: 1000,
+                    loading: true,
+                    content: msg
+                })
+            }
+        })
     }
-    
-    return botui.message.add({
-        delay: 1000,
-        loading: true,
-        content: message
-    })
 })
